52a17c9e5bc19242389eb6c28d99ebfc14cf8a80
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-20 20:27:33 -0700

    created post route, so user's input gets logged into playDataBase

--- app.js
+++ app.js
@@ -11,14 +11,46 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/playDB", {useNewUrlParser:true, useUnifiedTopology:true});
 
+const userSchema = {
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}
+
+const User = mongoose.model("User", userSchema)
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+
+app.post("/", function (req, res){
+    const user = new User ({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    console.log(user.username);
+    console.log(user.password);
+
+    user.save(function(err){
+        if (err){
+            console.log("Error");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
 let port = process.env.PORT;
 if(port == null || port == ""){
     port = 3000;
 }
 
-app.get("/", function(req,res){
-    res.render("index");
-});
 
 app.listen(port, function(){
     console.log("Server started and connected");
